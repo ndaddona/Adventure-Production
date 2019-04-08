f80cca9494e88b1cfd382230b1e698599f1867bc
CrazyFireFerret <nick@canc.com>
2019-04-08 15:43:02 -0500

    image scaling changes

--- app/helpers/campaigns_helper.rb
+++ app/helpers/campaigns_helper.rb
@@ -1,11 +1,4 @@
 module CampaignsHelper
 
-    def image_for_campaign(campaign, size)
-        if campaign.image_file_name.blank?
-          image_tag('d20.png', size)
-        else
-          image_tag(campaign.image_file_name, size)
-        end
-    end
 
 end
--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,9 +1,4 @@
 module GamesHelper
 
-    def image_for_game(game, size)
-        unless game.image_file_name.blank?
-          image_tag(game.image_file_name, size)
-        end
-    end
 
 end
