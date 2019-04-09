49c466f32336d9a152817b4ab0ae70ed4a0d2f0a
CrazyFireFerret <nick@canc.com>
2019-04-09 10:51:51 -0500

    migration change

--- db/migrate/20190409153523_create_signups.rb
+++ db/migrate/20190409153523_create_signups.rb
@@ -1,8 +1,8 @@
 class CreateSignups < ActiveRecord::Migration[5.2]
   def change
     create_table :signups do |t|
-      t.references :users, foreign_key: true
-      t.references :campaigns, foreign_key: true
+      t.references :user, foreign_key: true
+      t.references :campaign, foreign_key: true
     end
   end
 end
